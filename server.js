daf0ee5b5540b00b8b0f9894c69f46c3abc13b31
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-28 22:39:28 -0400

    added some routes to the server.js file....will eventually move them into their own file.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
@@ -14,4 +15,49 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mongoHeadlines")
+mongoose.connect("mongodb://localhost/mongoHeadlines");
+
+
+app.get("/scrape", function(req, res) {
+
+    axios.get("http://www.npr.org/").then(function(response) {
+        var cheer = cheerio.load(response.data);
+
+        cheer("h3.title").each(function(i, element) {
+            var collection = {};
+
+            collection.title = cheer(this)
+                .text();
+            collection.link = cheer(this)
+                .parent()
+                .attr("href");
+
+            db.Article.create(collection)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    return res.json(err);
+                });
+        });
+        location.reload();
+    });
+});
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    location.reload();
+});
+
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log(`App running on port ${PORT}!`);
+});
